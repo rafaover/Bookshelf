b69045105f5a7d8bb1d8243ea722333d27ac2ed9
rafaover <rafamsilva@gmail.com>
2024-02-24 12:29:40 +1100

    Created Topbar composable with scrollbehavior

---
+++ app/src/main/java/com/feeltheboard/bookshelf/ui/components/BookShelfAppTopBar.kt
@@ -0,0 +1,29 @@
+package com.feeltheboard.bookshelf.ui.components
+
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.feeltheboard.bookshelf.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BookShelfAppTopBar(
+    modifier: Modifier = Modifier
+) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(id = R.string.app_name),
+                style = MaterialTheme.typography.headlineSmall
+            )
+        },
+        modifier = modifier,
+        scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+    )
+}
