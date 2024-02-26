f3b6aba925706f8474c103a3a8d398881e045b58
rafaover <rafamsilva@gmail.com>
2024-02-26 20:27:00 +1100

    Minor Updates

--- app/src/main/java/com/feeltheboard/bookshelf/network/BooksApiService.kt
+++ app/src/main/java/com/feeltheboard/bookshelf/network/BooksApiService.kt
@@ -1,7 +1,6 @@
 package com.feeltheboard.bookshelf.network
 
 import com.feeltheboard.bookshelf.network.model.Books
-import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
@@ -13,13 +12,12 @@ val retrofit: Retrofit = Retrofit.Builder()
     .addConverterFactory(GsonConverterFactory.create())
     .build()
 
+interface BooksApiService {
+    @GET("v1/volumes?q=science+fiction/")
+    suspend fun getBooks(): Books
+}
 object BooksApiClient {
     val retrofitService: BooksApiService by lazy {
         retrofit.create(BooksApiService::class.java)
     }
-}
-
-interface BooksApiService {
-    @GET("v1/volumes?q=science+fiction/")
-    suspend fun getBooks(): Books
 }
