6f9eb68e4cb3a37e5587ef14e51b7debf773f695
rafaover <rafamsilva@gmail.com>
2024-02-26 17:59:37 +1100

    Added main composable to MainActivity.kt

--- app/src/main/java/com/feeltheboard/bookshelf/MainActivity.kt
+++ app/src/main/java/com/feeltheboard/bookshelf/MainActivity.kt
@@ -3,17 +3,9 @@ package com.feeltheboard.bookshelf
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import com.feeltheboard.bookshelf.ui.BookShelfApp
 import com.feeltheboard.bookshelf.ui.theme.BookshelfTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +16,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //
+                    BookShelfApp()
                 }
             }
         }
