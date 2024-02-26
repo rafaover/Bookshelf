cc134664bf4b0d2eeda13487bedd540cb8679731
rafaover <rafamsilva@gmail.com>
2024-02-26 13:42:29 +1100

    BookShelfViewModel created - Rightnow it's just acquiring the data and counting the book quantity

---
+++ app/src/main/java/com/feeltheboard/bookshelf/ui/BooksViewModel.kt
@@ -0,0 +1,23 @@
+package com.feeltheboard.bookshelf.ui
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.feeltheboard.bookshelf.data.NetworkBooksRepository
+import kotlinx.coroutines.launch
+
+class BookShelfViewModel: ViewModel() {
+    private var bookShelfState: String by mutableStateOf("")
+
+    init {
+        getBooks()
+    }
+    private fun getBooks() =
+        viewModelScope.launch {
+            val booksRepository = NetworkBooksRepository()
+            val books = booksRepository.getBooks()
+            bookShelfState = ("{$books.size} Books found!")
+        }
+}
