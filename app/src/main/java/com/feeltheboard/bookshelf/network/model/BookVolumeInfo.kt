b34147edc317999102b335839e531cb382986df6
rafaover <rafamsilva@gmail.com>
2024-02-26 18:14:06 +1100

    Updated Models - Removed unnecessary data.

--- app/src/main/java/com/feeltheboard/bookshelf/network/model/BookVolumeInfo.kt
+++ app/src/main/java/com/feeltheboard/bookshelf/network/model/BookVolumeInfo.kt
@@ -2,7 +2,5 @@ package com.feeltheboard.bookshelf.network.model
 
 data class BookVolumeInfo(
     val title: String,
-    val authors: List<String>,
-    val description: String,
     val imageLinks: BookImageLinks
 )
