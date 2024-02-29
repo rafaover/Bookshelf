e91d26e4f9eaeede2b1cbf25b3274be215ce12bc
rafaover <rafamsilva@gmail.com>
2024-03-01 10:09:57 +1100

    Updated ViewModel - REfactoring on Try/catch - Added dependency booksRepository to refactor fun getbooks()

--- app/src/main/java/com/feeltheboard/bookshelf/ui/BooksViewModel.kt
+++ app/src/main/java/com/feeltheboard/bookshelf/ui/BooksViewModel.kt
@@ -4,24 +4,28 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import com.feeltheboard.bookshelf.data.BooksRepository
 import com.feeltheboard.bookshelf.data.NetworkBooksRepository
 import kotlinx.coroutines.launch
 
-class BookShelfViewModel: ViewModel() {
+class BookShelfViewModel(
+    private val booksRepository: BooksRepository
+): ViewModel() {
     var bookShelfState: String by mutableStateOf("")
 
     init {
         getBooks()
     }
+
     private fun getBooks() =
-        viewModelScope.launch {
-            try {
-                val booksRepository = NetworkBooksRepository()
+        try {
+            viewModelScope.launch {
                 val books = booksRepository.getBooks()
                 bookShelfState = ("${books.items.size} Books found!")
-            } catch (e: Exception) {
-                bookShelfState = "Error: ${e.message}"
             }
+        } catch (e: Exception) {
+                bookShelfState = "Error: ${e.message}"
         }
 }
