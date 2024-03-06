34f49acc5c42f59be1127669f08844dd94f47e42
rafaover <rafamsilva@gmail.com>
2024-03-06 14:39:42 +1100

    Updated Viewmodel to save Thumbnail field for future use. - Added replace method to change http->https

--- app/src/main/java/com/feeltheboard/bookshelf/ui/BooksViewModel.kt
+++ app/src/main/java/com/feeltheboard/bookshelf/ui/BooksViewModel.kt
@@ -4,16 +4,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.feeltheboard.bookshelf.data.BooksRepository
-import com.feeltheboard.bookshelf.data.NetworkBooksRepository
 import kotlinx.coroutines.launch
 
 class BookShelfViewModel(
     private val booksRepository: BooksRepository
 ): ViewModel() {
     var bookShelfState: String by mutableStateOf("")
+    var bookShelfThumbnail: List<String> by mutableStateOf(emptyList())
 
     init {
         getBooks()
@@ -24,6 +23,9 @@ class BookShelfViewModel(
             viewModelScope.launch {
                 val books = booksRepository.getBooks()
                 bookShelfState = ("${books.items.size} Books found!")
+                bookShelfThumbnail = books.items.map {
+                    it.volumeInfo.imageLinks.thumbnail.replace("http", "https")
+                }
             }
         } catch (e: Exception) {
                 bookShelfState = "Error: ${e.message}"
