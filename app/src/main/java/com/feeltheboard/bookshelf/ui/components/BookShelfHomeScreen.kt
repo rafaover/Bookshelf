e8984bf8b7cc172cef5c77fe5edda96206cc4c82
rafaover <rafamsilva@gmail.com>
2024-03-06 14:41:10 +1100

    Created composable LazyStaggeredGrid to show thumbnails.

--- app/src/main/java/com/feeltheboard/bookshelf/ui/components/BookShelfHomeScreen.kt
+++ app/src/main/java/com/feeltheboard/bookshelf/ui/components/BookShelfHomeScreen.kt
@@ -1,26 +1,29 @@
 package com.feeltheboard.bookshelf.ui.components
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.feeltheboard.bookshelf.network.model.Books
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun BookShelfHomeScreen(
-    books: String,
+    bookThumbs: List<String>,
     contentPadding: PaddingValues,
     modifier: Modifier = Modifier
     ) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(books)
+    LazyVerticalStaggeredGrid(
+        columns = StaggeredGridCells.Adaptive(160.dp),
+        verticalItemSpacing = 4.dp,
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        contentPadding = contentPadding,
+        content = {
+            items(bookThumbs) { thumb ->
+                BookItem(thumb)
+            }
         }
-    }
+    )
+}
